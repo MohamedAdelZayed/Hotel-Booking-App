79653b543325e6d1b4e1c3f22d6e2f57b5d0319d
mohamed zayed <zayd0497@gmail.com>
2025-08-02 19:04:28 +0300

    add btn to scroll to top

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -3,9 +3,11 @@ import FeaturedDestination from '../FeaturedDestination/FeaturedDestination'
 import Offers from '../Offers/Offers'
 import Clients from '../Clients/Clients'
 import Newsletter from '../Newsletter/Newsletter'
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+
+  const [scroll, setScroll] = useState(false)
 
   useEffect( ()=>{
   
@@ -13,15 +15,47 @@ export default function Home() {
       
   
   } , [])
+
+
+  useEffect(()=> {
+
+    const handleScroll = ()=> {
+
+    if( window.scrollY >= 400  ){
+       setScroll( true )
+    }else{
+      setScroll( false )
+    }
+
+    }
+
+    window.addEventListener( "scroll" , handleScroll );
+
+    return () => window.removeEventListener("scroll", handleScroll);
+
+  } , []) 
  
   return (
     <>
+    <div className = "relative">
+
+      <div 
+     
+      onClick = {()=> window.scrollTo({top : 0 , behavior : 'smooth'  }) }
+     
+      className={` ${ scroll === true ? 'fixed' : 'hidden'  } bottom-1  right-2 z-50 bg-gray-800 rounded-full cursor-pointer p-2 shadow-lg animate-bounce `}>
+     
+      👆
+     
+     </div>
 
       <HeroSection/>
       <FeaturedDestination/>
       <Offers/>
       <Clients/>
       <Newsletter/>
+
+      </div>
           
 
 
